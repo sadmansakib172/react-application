a768c207e3f4c1f10c2ad364d4f45892c64e9526
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-09-24 17:43:58 +0600

    data load for home page done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,15 @@
+import { useLoaderData } from "react-router-dom";
+import Banner from "../../components/Header/Banner/Banner";
+import Phones from "../../components/Phones/Phones";
 
 
 const Home = () => {
+    const phones = useLoaderData()
+    console.log(phones)
     return (
         <div>
-            Home
+            <Banner></Banner>
+            <Phones></Phones>
         </div>
     );
 };
---
+++ src/components/Phones/Phones.jsx
@@ -0,0 +1,11 @@
+
+
+const Phones = () => {
+    return (
+        <div className="py-10">
+            <h1 className="text-2xl text-center">All category phones</h1>
+        </div>
+    );
+};
+
+export default Phones;
